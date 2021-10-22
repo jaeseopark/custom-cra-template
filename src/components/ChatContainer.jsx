39bc75ee1e97d5dc64ce0a7d1e635997d52659cc
Nathan Nguyen <lenathan.nguyen@harness.io>
2021-10-22 01:07:56 -0600

    feat: added chat wireframe

---
+++ src/components/ChatContainer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+
+import ContactContainer from "./ContactContainer";
+
+const ChatContainer = () => {
+	// Populate this with your GET contacts service call
+	const contacts = ["Nathan", "Jacky", "Jaeseo"];
+
+	function getContactContainers() {
+		const contactContainers = contacts.map((contact, index) => {
+			return (
+				<React.Fragment key={index}>
+					<ContactContainer contactName={contact} />
+					{index < contacts.length - 1 ? <Divider /> : null}
+				</React.Fragment>
+			);
+		});
+
+		return contactContainers;
+	}
+
+	return (
+		<div className="chatContainer">
+			<List component="nav" aria-label="mailbox folders" className="list">
+				{getContactContainers()}
+			</List>
+		</div>
+	);
+};
+
+export default ChatContainer;
