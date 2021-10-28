06c7c20b224f86e8038583e98bccfa8f5b041c54
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-10-28 17:51:39 -0600

    fix: docker

--- src/redux/mdlwr.ts
+++ src/redux/mdlwr.ts
@@ -8,7 +8,7 @@ import { updateConnectivity as updateSliceConnectivity } from "redux/connectivit
 let imfClient: IMFClient;
 
 export const initializeClient = () => (dispatch: Dispatch) => {
-    const host = process.env.REACT_APP_SERVER_HOST!;
+    const host = process.env.SERVER_HOST || (process.env.REACT_APP_SERVER_HOST!);
     const port = process.env.REACT_APP_SERVER_PORT!;
     const client = new IMFClient(host, port);
     client.onEvent((event) => {
